7316d2125f848914e90b26d532dc2165db4054bf
Alexander Lopatin <alopatindev@gmail.com>
2016-12-12 20:33:01 +0300

    update send_kcov.sh

--- scripts/send_kcov.sh
+++ scripts/send_kcov.sh
@@ -8,17 +8,16 @@ PKGNAMEVER="${PKGID#*#}"
 PKGNAME="$(echo $ORIGIN | sed 's!.*\/!!')"
 shift
 cargo test --no-run || exit $?
-EXE=($ORIGIN/target/debug/$PKGNAME-*)
-if [ ${#EXE[@]} -ne 1 ]; then
-    echo 'Non-unique test file, retrying...' >2
-    rm -f ${EXE[@]}
-    cargo test --no-run || exit $?
-fi
 
-kcov/build/src/kcov \
-    --exclude-pattern=/.cargo,/usr/lib \
-    --verify \
-    --coveralls-id=$TRAVIS_JOB_ID \
-    $ORIGIN/target/cov \
-    $ORIGIN/target/debug/$PKGNAME-* \
-    "$@"
+for i in $ORIGIN/target/debug/${PKGNAME}*
+do
+    echo "send_kcov: $i"
+    OUTDIR="target/cov/$(basename $i)"
+    mkdir -p "${OUTDIR}"
+    kcov/build/src/kcov \
+        --exclude-pattern=/.cargo,/usr/lib \
+        --verify \
+        --coveralls-id=${TRAVIS_JOB_ID} \
+        "${OUTDIR}" \
+        "$i"
+done
